1deda227650f436c2af6123636784b4e53728ddd
Turmunkh07 <108219155+Turmunkh07@users.noreply.github.com>
2023-05-24 09:58:16 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 function validate() {
   let username = document.getElementById("username").value;
   let pwd = document.getElementById("password").value;
-  if (username == "admin" && pwd == "user") {
+  if (username == "SE000000" && pwd == "ADA123456") {
     alert("login succesfully");
-    location.replace('index.html');
+    location.replace('/ada')
   } else {
     alert("login failed");
   }
