12db1617c3be2d1db978fc26d0b08e7596c3b7a0
Turmunkh07 <108219155+Turmunkh07@users.noreply.github.com>
2023-05-24 10:38:56 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 function validate() {
-  let username = document.getElementById("username").value;
+  let serialNum = document.getElementById("snum").value;
   let pwd = document.getElementById("password").value;
-  if (username == "SE000000" && pwd == "ADA123456") {
+  if (serialNum == "SE000000" && pwd == "ADA123456") {
     alert("login succesfully");
-    location.replace('/ada')
+    window.location.assign('turmunkh07.github.io/ada')
   } else {
     alert("login failed");
   }
